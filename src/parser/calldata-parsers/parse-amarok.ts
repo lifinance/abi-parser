ae0b43b453886d90607439fd5c1b540f25677422
lukas <git@lukaselsner.de>
2023-08-03 17:05:59 +0200

    fix

--- src/parser/calldata-parsers/parse-amarok.ts
+++ src/parser/calldata-parsers/parse-amarok.ts
@@ -12,7 +12,7 @@ export const parseAmarok = (encodedCallData: string): Array<CallDataInformation>
     try {
         const functionParameters = AbiCoder.defaultAbiCoder().decode(AMAROK_PAYLOAD_ABI, hexify(encodedCallData))
 
-        // For currently unknown reason, `decode` somethimes returns an object where accessing the first element fails
+        // For currently unknown reason, `decode` sometimes returns an object where accessing the first element fails
         // This is a workaround for that
         try {
             const [xxx] = functionParameters
