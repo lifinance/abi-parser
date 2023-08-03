5f80b4518691efd7b7e4c43961305a1d059d1e39
lukas <git@lukaselsner.de>
2023-08-03 17:51:22 +0200

    chore(eslint): Conditionally disable linter on bigint patch

--- src/bigint/patch-bigint.ts
+++ src/bigint/patch-bigint.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/* eslint-disable */
 // @ts-ignore
-export const patchBigint = (): (() => string) =>
-  ((BigInt.prototype as any).toJSON = () => this.toString())
+export const patchBigint = (): (() => string) => ((BigInt.prototype as any).toJSON = () => this.toString())
+/* eslint-enable */
