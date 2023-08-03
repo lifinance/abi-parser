07e47c17a8e407bc4b7f77e6d4de96570a2d8372
lukas <git@lukaselsner.de>
2023-08-03 14:05:52 +0200

    add more chains

--- src/chains/init-chains.ts
+++ src/chains/init-chains.ts
@@ -6,5 +6,7 @@ import { createChain } from './create-chain'
 export const initChains = (): Array<ChainConfig> => [
     createChain(ChainId.ETH, 'api.etherscan.io', process.env.ETH_API_KEY),
     createChain(ChainId.POL, 'api.polygonscan.com', process.env.POL_API_KEY),
-    createChain(ChainId.ARB, 'api.arbiscan.io', process.env.ARB_API_KEY)
+    createChain(ChainId.ARB, 'api.arbiscan.io', process.env.ARB_API_KEY),
+    createChain(ChainId.OPT, 'api-optimistic.etherscan.io', process.env.OPT_API_KEY),
+    createChain(ChainId.AVA, 'api.snowtrace.io', process.env.AVA_API_KEY)
 ]
